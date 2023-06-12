fd4a845f06040e6ec6fc3e9e101bd17a2e6959a7
NovichenkoEvgeniy <tnrkip@yandex.ru>
2023-06-12 22:35:08 +0300

    add fun del and change

--- logger.py
+++ logger.py
@@ -63,12 +63,32 @@ def put_data():
         number_journal -= 1
         # Можно добавить проверку, чтобы человек не выходил за пределы записей
         print(f'Изменить данную запись\n{data_first[number_journal]}')
-        name = name_data()
-        surname = surname_data()
-        phone = phone_data()
-        address = address_data()
-        data_first = data_first[:number_journal] + [f'{name}\n{surname}\n{phone}\n{address}\n'] + \
-                     data_first[number_journal + 1:]
+
+        data_temp=data_first[number_journal].split()
+        vibor_polya=int(input("Что хотите поменять: 1.Имя 2.Фамилию 3.Номер телефона 4.Город"))
+
+        if vibor_polya ==1:
+            data_first = data_first[:number_journal] + [f'{name_data()}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya==2:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{surname_data()}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 3:
+
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{phone_data()}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 4:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{address_data()}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 5:
+            data_first = data_first[:number_journal] + [
+                f'{name_data()}\n{surname_data()}\n{phone_data()}\n{address_data()}\n'] + \
+                         data_first[number_journal + 1:]
+
+
         with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_first))
         print('Изменения успешно сохранены!')
@@ -78,12 +98,29 @@ def put_data():
         number_journal -= 1
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Изменить данную запись\n{data_second[number_journal]}')
-        name = name_data()
-        surname = surname_data()
-        phone = phone_data()
-        address = address_data()
-        data_second = data_second[:number_journal] + [f'{name};{surname};{phone};{address}\n'] + \
+        data_temp=data_second[number_journal].split(';')
+        vibor_polya=int(input("Что хотите поменять: 1.Имя 2.Фамилию 3.Номер телефона 4.Город 5.Всю "))
+
+        if vibor_polya == 1:
+            data_second = data_second[:number_journal] + [
+                f'{name_data()};{data_temp[1]};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif vibor_polya == 2:
+            data_second = data_second[:number_journal] + [
+                f'{data_temp[0]};{surname_data()};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif vibor_polya == 3:
+
+            data_second = data_second[:number_journal] + [
+                f'{data_temp[0]};{data_temp[1]};{phone_data()};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif vibor_polya == 4:
+            data_second = data_second[:number_journal] + [f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{address_data()}\n'] + \
                       data_second[number_journal + 1:]
+        elif vibor_polya == 5:
+            data_second = data_second[:number_journal] + [f'{name_data()};{surname_data()};{phone_data()};{address_data()}\n'] + \
+                      data_second[number_journal + 1:]
+
         with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_second))
         print('Изменения успешно сохранены!')  # Можно вывести конечные данные
@@ -103,7 +140,30 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_first[number_journal - 1]}')
-        data_first = data_first[:number_journal] + data_first[number_journal + 1:]
+        data_temp=data_first[number_journal].split()
+        vibor_polya = int(input("Что хотите удалить: 1.Имя 2.Фамилию 3.Номер телефона 4.Город 5.Всю "))
+
+        if vibor_polya == 1:
+            data_first = data_first[:number_journal] + [
+                f'{""}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 2:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{""}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 3:
+
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{""}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 4:
+            data_first = data_first[:number_journal] + [
+                f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{""}\n'] + \
+                         data_first[number_journal + 1:]
+        elif vibor_polya == 5:
+            data_first = data_first[:number_journal] + data_first[number_journal + 1:]
+
+
         with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_first))
         print('Изменения успешно сохранены!')
@@ -112,7 +172,30 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_second[number_journal - 1]}')
-        data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+        print(data_second)
+        data_temp = data_second[number_journal-1].split(';')
+        vibor_polya = int(input("Что хотите удалить: 1.Имя 2.Фамилию 3.Номер телефона 4.Город 5.Всю "))
+
+        if vibor_polya == 1:
+            data_second = data_second[:number_journal-1] + [
+                f'{""};{data_temp[1]};{data_temp[2]};{data_temp[3]}'] + \
+                          data_second[number_journal:]
+        elif vibor_polya == 2:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{""};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal:]
+        elif vibor_polya == 3:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{data_temp[1]};{""};{data_temp[3]}\n'] + \
+                          data_second[number_journal:]
+        elif vibor_polya == 4:
+            data_second = data_second[:number_journal-1] + [
+                f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{""}\n'] + \
+                          data_second[number_journal:]
+        elif vibor_polya == 5:
+            data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+
+        init =input('pause')
         with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_second))
         print('Изменения успешно сохранены!')  # Можно вывести конечные данные
